bd2ffe0113a2f60cbe46ab97ce94d4e3cdc2cc91
Victor Agbeo <vicagbeo@outlook.com>
2023-09-07 19:26:39 -0700

    cart comp and bag svg

---
+++ src/components/cart/cartIcon.jsx
@@ -0,0 +1,13 @@
+import { ReactComponent as ShoppingIcon } from "react";
+
+import '../cart.scss';
+
+const Cart = () => {
+    return(
+        <div>
+            <ShoppingIcon />
+        </div>
+    )
+}
+
+export default Cart;
