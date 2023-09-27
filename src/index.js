b7de255301b2a4792f5184d843735b90bc727b26
Victor Agbeo <vicagbeo@outlook.com>
2023-09-26 17:01:34 -0700

    gh setup

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App'
 import {UserProvider} from './context/User-Context'
 import {ProductsProvider} from './context/Products'
-
+import { HashRouter as Router } from "react-router-dom";
 import './index.scss'
 
 
@@ -13,16 +13,24 @@ import './index.scss'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <UserProvider>
         <ProductsProvider>
           <App />
         </ProductsProvider>
       </UserProvider>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
-
+{/* <React.StrictMode> */}
+{/* <BrowserRouter>
+  <UserProvider>
+    <ProductsProvider>
+      <App />
+    </ProductsProvider>
+  </UserProvider>
+</BrowserRouter>
+</React.StrictMode> */}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
