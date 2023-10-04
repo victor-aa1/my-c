09e326585debafbc55bbbdd8e941894b9cf36086
victor.agbeo <vicagbeo@outlook.com>
2023-10-03 17:08:56 -0700

    configuring routing

--- src/App.js
+++ src/App.js
@@ -1,27 +1,26 @@
 
-import { Routes, Route, HashRouter } from 'react-router-dom';
+import { Route, Router  } from 'react-router-dom';
 
 import Navigation from './routes/Navbar/Nav-bar'
 import Home from './routes/home/home-comp';
 import Authentication from './routes/authentication/Authentication';
 import Shop from './routes/shop/Shop'
 
-
-const App =() => {
-  
+// Use hashrouter to route links
+const App = () => {
   return(
-    <HashRouter>
-      <Routes>
-        <Route path='/' element={<Navigation />}>
-          
-          <Route index element={<Home />}/>
-          {/* displays def home comp when path is set to index */}
-          
-          <Route path='auth' element={<Authentication />} />
-          <Route path='shop' element={<Shop />}/>
-      </Route>
-      </Routes>
-    </HashRouter>
+  
+        <Router>
+          <Route path='/' element={<Navigation />}>
+            
+            <Route index element={<Home />}/>
+            {/* displays def home comp when path is set to index */}
+            
+            <Route path='auth' element={<Authentication />} />
+            <Route path='shop' element={<Shop />}/>
+          </Route>
+        </Router>
+      
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './App'
 import {UserProvider} from './context/User-Context'
 import {ProductsProvider} from './context/Products'
-import { HashRouter } from "react-router-dom";
 import './index.scss'
 
+// Use hashrouter to route links
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -22,15 +22,15 @@ root.render(
     </HashRouter>
   </React.StrictMode>
 );
-{/* <React.StrictMode> */}
-{/* <BrowserRouter>
-  <UserProvider>
-    <ProductsProvider>
-      <App />
-    </ProductsProvider>
-  </UserProvider>
-</BrowserRouter>
-</React.StrictMode> */}
+// {/* <React.StrictMode> */}
+// {/* <BrowserRouter>
+//   <UserProvider>
+//     <ProductsProvider>
+//       <App />
+//     </ProductsProvider>
+//   </UserProvider>
+// </BrowserRouter>
+// </React.StrictMode> */}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
